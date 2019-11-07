e438391a6253c5ffc124307bd98fcd5553bae445
Glitch (hello-webpage) <none>
2019-11-07 04:58:46 +0000

    📒🌵 Checkpoint ./script.js:7300773/6187

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
-$.getJSON( "https://api.exchangerate-api.com/v4/latest/USD", function( json ) {
-  var infoUsd = json; 
+function convert() {
+  var input = document.getElementById("input1");
+  var amount = input.value;
+  var select1 = document.getElementById("sel1");
+  var currency1 = select1.options[select1.selectedIndex].value;
+  var select2 = document.getElementById("sel2");
+  var currency2 = select1.options[select2.selectedIndex].value;
+  var info = $.getJSON(
+    "https://api.exchangerate-api.com/v4/latest/USD",
+    convertFor(amount, currency1, currency2)
+  );
+}
 
-  function convert() {
-    var input = document.getElementById("input1");
-    var amount = input.value;
-    var currency1 = select1.options[select1.selectedIndex].value;
-    var select1 = document.getElementById("sel1");
-    var currency1 = select1.options[select1.selectedIndex].value;
-    var select2 = document.getElementById("sel2");
-    var currency2 = select1.options[select2.selectedIndex].value;
-    document.getElementById("input1").innerHTML = amount * infoUsd.currency2 / infoUsd.currency1;
-  }
-  
-});
-
+function convertFor(info, amount, currency1, currency2){
+  document.getElementById("result").innerHTML = amount * info.results[0].currency2 / info.results[0].currency1;
+}
