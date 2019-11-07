27b2214289234782b7ddcdae5ded973b140c33d9
Glitch (hello-webpage) <none>
2019-11-07 05:10:46 +0000

    ⛰🍍 Checkpoint ./index.html:7300773/6727 ./script.js:7300773/1627

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-function convert() {
-  var input = document.getElementById("input1");
-  var amount = input.value;
-  var select1 = document.getElementById("sel1");
-  var currency1 = select1.options[select1.selectedIndex].value;
-  var select2 = document.getElementById("sel2");
-  var currency2 = select1.options[select2.selectedIndex].value;
-  var info = $.getJSON(
-    "https://api.exchangerate-api.com/v4/latest/USD",
-    convertFor(amount, currency1, currency2)
-  );
-}
 
-function convertFor(info, amount, currency1, currency2){
-  document.getElementById("result").innerHTML = amount * info.results[0].currency2 / info.results[0].currency1;
-}
+  function convert() {
+    var input = document.getElementById("amount");
+    var amount = input.value;
+    var select1 = document.getElementById("sel");
+    var currency1 = select1.options[select1.selectedIndex].value;
+    var select2 = document.getElementById("sel2");
+    var currency2 = select1.options[select2.selectedIndex].value;
+
+    var info = $.getJSON(
+      "https://api.exchangerate-api.com/v4/latest/USD",
+      convertFor(amount, currency1, currency2)
+    );
+  }
+
+  function convertFor(info, amount, currency1, currency2) {
+    console.log(amount);
+    console.log(info.currency1);
+    console.log(info.currency2);
+    document.getElementById("result").innerHTML =
+      (amount * info.currency2) / info.currency1;
+  }
+
