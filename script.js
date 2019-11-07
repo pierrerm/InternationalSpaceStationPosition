22faef829271b7f09e48c54ca028d00f6ff5d335
Glitch (hello-webpage) <none>
2019-11-07 04:32:55 +0000

    ✍️⛵️ Checkpoint ./script.js:7300773/679 ./index.html:7300773/1539

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 $.getJSON( "https://api.exchangerate-api.com/v4/latest/USD", function( json ) {
-  var infoUsd = json;  
+  var infoUsd = json; 
 
   $('#sel').append('<option value="' + infoUsd.base + '">' + infoUsd.base + '</option>');
 });
