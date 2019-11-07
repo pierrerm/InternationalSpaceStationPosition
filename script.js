b750ddf47917bc96dfb1ba7b56f3d21bb01ee661
Glitch (hello-webpage) <none>
2019-11-07 04:44:54 +0000

    🌊🏯 Checkpoint ./index.html:7300773/277 ./script.js:7300773/799

--- script.js
+++ script.js
@@ -3,5 +3,16 @@
 $.getJSON( "https://api.exchangerate-api.com/v4/latest/USD", function( json ) {
   var infoUsd = json; 
 
-  $('#sel').append('<option value="' + infoUsd.base + '">' + infoUsd.base + '</option>');
+  function convert() {
+    var input = document.getElementById("input1");
+    var amount = input.value;
+    var currency1 = select1.options[select1.selectedIndex].value;
+    var select1 = document.getElementById("sel1");
+    var currency1 = select1.options[select1.selectedIndex].value;
+    var select2 = document.getElementById("sel2");
+    var currency2 = select1.options[select2.selectedIndex].value;
+    document.getElementById("input1").innerHTML = amount * infoUsd.currency2 / infoUsd.currency1;
+  }
+  
 });
+
